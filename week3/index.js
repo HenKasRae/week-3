c4a8fa3ddea3cf92714e68600a2240668f6d711b
HenKasRae <henry.kasak7@gmail.com>
2025-05-12 19:32:16 +0300

    index.js

--- week3/index.js
+++ week3/index.js
@@ -7,15 +7,6 @@ btnSubmit.addEventListener('click', () => {
     const usernameInput = usernameInputField.value.toLowerCase(); //HARRY/ HaRRy / hArRy==> harry
     const userAgeInput = userAgeInputField.value;
 
-    // if(usernameInput === 'harry' && userAgeInput === '11') {
-    //     console.log('welcome');
-    //     loginMessage.textContent = 'Welcome, Harry!';
-    //     loginMessage.style.color = 'green';
-    // } else {
-    //     loginMessage.textContent = 'Go away! You are not Harry!';
-    //     loginMessage.style.color = 'red';
-    // }
-
     if(usernameInput !== 'harry' || userAgeInput !== '11') {
         loginMessage.textContent = 'Go away! You are not Harry!';
         loginMessage.style.color = 'red';
